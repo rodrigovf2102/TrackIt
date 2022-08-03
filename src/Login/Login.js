7298fc37a1b7c67abe24308de501c770898895d2
Rodrigo <rodrigovf2102@gmail.com>
2022-08-03 17:58:29 -0300

    tela login funcional

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,44 +1,53 @@
 import Icone from '../Assets/Img/Icone.png'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { postLogin } from '../Services/TrackIt';
 import { useState } from 'react';
+import { Grid } from 'react-loader-spinner';
 
 export default function Login() {
 
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [corEntrar,setCorEntrar] = useState(1)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [corEntrar, setCorEntrar] = useState(1)
+    const [disableForm, setDisableForm] = useState(false);
 
     function loginInfo(event) {
         event.preventDefault();
     }
 
-    function Login(){
-        setCorEntrar(0.7)
-        const login = { email: email, password:password};
+    function Login() {
+        setCorEntrar(0.7);
+        setDisableForm(true);
+        const login = { email: email, password: password };
         const promisse = postLogin(login);
 
         promisse.then(Autorizado);
         promisse.catch(Desautorizado);
     }
 
-    function Desautorizado(erro){
-        console.log(erro.response.status);
-        setCorEntrar(1)
+    function Desautorizado() {
+        alert("Usuário ou senha incorreto");
+        setCorEntrar(1);
+        setDisableForm(false);
     }
 
-    function Autorizado(){
-
+    function Autorizado() {
+        setDisableForm(false);
+        useNavigate("/hoje");
     }
 
     return (
         <Container>
             <img src={Icone} />
             <Form onSubmit={loginInfo}>
-                <Input type="text" placeholder=' Email' onChange={event => setEmail(event.target.value)} required />
-                <Input type="password" placeholder=' Senha' onChange={event => setPassword(event.target.value)} required/>
-                <Entrar cor={corEntrar} onClick={Login} type="submit">{"Entrar"} </Entrar>
+                <Input type="text" placeholder=' Email' onChange={event => setEmail(event.target.value)} 
+                                   disabled={disableForm} required />
+                <Input type="password" placeholder=' Senha' onChange={event => setPassword(event.target.value)} 
+                                   disabled={disableForm} required />
+                <Entrar cor={corEntrar} onClick={Login} disabled={disableForm} type="submit">
+                    {disableForm ? <Grid color='white' radius="8" heigth="100"/> :"Entrar"}
+                </Entrar>
                 <Link to="/cadastro"><Cadastro>Não tem uma conta? Cadastre-se</Cadastro></Link>
             </Form>
         </Container>
@@ -76,6 +85,10 @@ border-radius: 5px;
 color: white;
 font-size: 21px;
 opacity: ${props => props.cor};
+display: flex;
+justify-content: center;
+align-items: center;
+overflow-y: hidden;
 `
 
 const Cadastro = styled.div`
